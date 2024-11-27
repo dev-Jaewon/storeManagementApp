3b284637ec67a639aed9f33cb5525a6d62a1a91e
dev-jaewon <app235naver@gmail.com>
2024-11-28 01:50:34 +0900

    feat icon aseets svg to component

---
+++ scripts/createIconComponent.ts
@@ -0,0 +1,37 @@
+import fs from 'fs';
+import path from 'path';
+import { pascalCase } from 'change-case-all';
+
+const svgDir = path.join(__dirname, '../assets/icons');
+const outputFile = path.join(__dirname, '../src/ui/common/Icons.tsx');
+
+const svgFiles = fs.readdirSync(svgDir).filter(file => file.endsWith('.svg'));
+
+// Generate the component code
+let componentCode = `import React from 'react';
+import { SvgProps } from 'react-native-svg';
+`;
+
+// Import each SVG component
+svgFiles.forEach(file => {
+  const componentName = pascalCase(file.replace('.svg', '').replace(/(\d+)\s+/, '').trim() + file.match(/\d+/)?.[0] || '');
+  componentCode += `import ${componentName} from '@/assets/icons/${file}';\n`;
+});
+
+componentCode += `\nexport const Icons = {\n`;
+
+// Add SVG components to Icons object with original filenames as keys
+svgFiles.forEach(file => {
+  const componentName = pascalCase(file.replace('.svg', '').replace(/(\d+)\s+/, '').trim() + file.match(/\d+/)?.[0] || '');
+  
+  componentCode += `  ${componentName}: (props: SvgProps) => (
+    <${componentName} {...props} />
+  ),\n`;
+});
+
+componentCode += `};\n\n`;
+
+// Write the generated code to the output file
+fs.writeFileSync(outputFile, componentCode);
+
+console.log('Icon components generated successfully!');
---
+++ src/ui/common/Icons.tsx
@@ -0,0 +1,406 @@
+import React from 'react';
+import { SvgProps } from 'react-native-svg';
+import User2 from '@/assets/icons/2 User.svg';
+import User3 from '@/assets/icons/3 User.svg';
+import Activityundefined from '@/assets/icons/Activity.svg';
+import AddUserundefined from '@/assets/icons/Add User.svg';
+import Avatarundefined from '@/assets/icons/Avatar.svg';
+import Bag_22 from '@/assets/icons/Bag 2.svg';
+import Bag_33 from '@/assets/icons/Bag 3.svg';
+import Bagundefined from '@/assets/icons/Bag.svg';
+import Bookmarkundefined from '@/assets/icons/Bookmark.svg';
+import Buyundefined from '@/assets/icons/Buy.svg';
+import Calendarundefined from '@/assets/icons/Calendar.svg';
+import CallMissedundefined from '@/assets/icons/Call Missed.svg';
+import CallSilentundefined from '@/assets/icons/Call Silent.svg';
+import Callundefined from '@/assets/icons/Call.svg';
+import Callingundefined from '@/assets/icons/Calling.svg';
+import Cameraundefined from '@/assets/icons/Camera.svg';
+import Cardundefined from '@/assets/icons/Card.svg';
+import Categoryundefined from '@/assets/icons/Category.svg';
+import Chartundefined from '@/assets/icons/Chart.svg';
+import Chat_22 from '@/assets/icons/Chat 2.svg';
+import Chatundefined from '@/assets/icons/Chat.svg';
+import CloseSquareundefined from '@/assets/icons/Close Square.svg';
+import DangerCircleundefined from '@/assets/icons/Danger Circle.svg';
+import DangerTriangleundefined from '@/assets/icons/Danger Triangle.svg';
+import Delete_22 from '@/assets/icons/Delete 2.svg';
+import Deleteundefined from '@/assets/icons/Delete.svg';
+import Discountundefined from '@/assets/icons/Discount.svg';
+import Discoveryundefined from '@/assets/icons/Discovery.svg';
+import Documentundefined from '@/assets/icons/Document.svg';
+import Download_11 from '@/assets/icons/Download-1.svg';
+import Downloadundefined from '@/assets/icons/Download.svg';
+import EditSquareundefined from '@/assets/icons/Edit Square.svg';
+import Editundefined from '@/assets/icons/Edit.svg';
+import Emojiundefined from '@/assets/icons/Emoji.svg';
+import Filter_33 from '@/assets/icons/Filter 3.svg';
+import Filterundefined from '@/assets/icons/Filter.svg';
+import Folderundefined from '@/assets/icons/Folder.svg';
+import Gameundefined from '@/assets/icons/Game.svg';
+import Graphundefined from '@/assets/icons/Graph.svg';
+import Heartundefined from '@/assets/icons/Heart.svg';
+import Heart22 from '@/assets/icons/Heart2.svg';
+import Hideundefined from '@/assets/icons/Hide.svg';
+import Home_22 from '@/assets/icons/Home 2.svg';
+import Homeundefined from '@/assets/icons/Home.svg';
+import Image_33 from '@/assets/icons/Image 3.svg';
+import Imageundefined from '@/assets/icons/Image.svg';
+import InfoSquareundefined from '@/assets/icons/Info Square.svg';
+import Location_22 from '@/assets/icons/Location 2.svg';
+import Locationundefined from '@/assets/icons/Location.svg';
+import Lockundefined from '@/assets/icons/Lock.svg';
+import Login_11 from '@/assets/icons/Login-1.svg';
+import Loginundefined from '@/assets/icons/Login.svg';
+import Logout_11 from '@/assets/icons/Logout-1.svg';
+import Logoutundefined from '@/assets/icons/Logout.svg';
+import Messageundefined from '@/assets/icons/Message.svg';
+import MoreCircleundefined from '@/assets/icons/More Circle.svg';
+import MoreSquareundefined from '@/assets/icons/More Square.svg';
+import Notification_22 from '@/assets/icons/Notification 2.svg';
+import Notification_33 from '@/assets/icons/Notification 3.svg';
+import Notificationundefined from '@/assets/icons/Notification.svg';
+import PaperDownload_11 from '@/assets/icons/Paper Download-1.svg';
+import PaperDownloadundefined from '@/assets/icons/Paper Download.svg';
+import PaperFailundefined from '@/assets/icons/Paper Fail.svg';
+import PaperNegativeundefined from '@/assets/icons/Paper Negative.svg';
+import PaperPlusundefined from '@/assets/icons/Paper Plus.svg';
+import PaperUpload_11 from '@/assets/icons/Paper Upload-1.svg';
+import PaperUploadundefined from '@/assets/icons/Paper Upload.svg';
+import Paperundefined from '@/assets/icons/Paper.svg';
+import Passwordundefined from '@/assets/icons/Password.svg';
+import Playundefined from '@/assets/icons/Play.svg';
+import Plusundefined from '@/assets/icons/Plus.svg';
+import Profileundefined from '@/assets/icons/Profile.svg';
+import QRundefined from '@/assets/icons/QR.svg';
+import Scanundefined from '@/assets/icons/Scan.svg';
+import Searchundefined from '@/assets/icons/Search.svg';
+import Sendundefined from '@/assets/icons/Send.svg';
+import Setting_22 from '@/assets/icons/Setting 2.svg';
+import Settingundefined from '@/assets/icons/Setting.svg';
+import ShieldDoneundefined from '@/assets/icons/Shield Done.svg';
+import ShieldFailundefined from '@/assets/icons/Shield Fail.svg';
+import Showundefined from '@/assets/icons/Show.svg';
+import Star_22 from '@/assets/icons/Star 2.svg';
+import Starundefined from '@/assets/icons/Star.svg';
+import Swapundefined from '@/assets/icons/Swap.svg';
+import TickSquareundefined from '@/assets/icons/Tick Square.svg';
+import TicketStarundefined from '@/assets/icons/Ticket Star.svg';
+import Ticketundefined from '@/assets/icons/Ticket.svg';
+import TimeCircleundefined from '@/assets/icons/Time Circle.svg';
+import TimeSquareundefined from '@/assets/icons/Time Square.svg';
+import Unlockundefined from '@/assets/icons/Unlock.svg';
+import Upload_11 from '@/assets/icons/Upload-1.svg';
+import Uploadundefined from '@/assets/icons/Upload.svg';
+import Videoundefined from '@/assets/icons/Video.svg';
+import Voice_33 from '@/assets/icons/Voice 3.svg';
+import Voiceundefined from '@/assets/icons/Voice.svg';
+import VolumeDownundefined from '@/assets/icons/Volume Down.svg';
+import VolumeOffundefined from '@/assets/icons/Volume Off.svg';
+import VolumeUpundefined from '@/assets/icons/Volume Up.svg';
+import Walletundefined from '@/assets/icons/Wallet.svg';
+import Workundefined from '@/assets/icons/Work.svg';
+
+export const Icons = {
+  User2: (props: SvgProps) => (
+    <User2 {...props} />
+  ),
+  User3: (props: SvgProps) => (
+    <User3 {...props} />
+  ),
+  Activityundefined: (props: SvgProps) => (
+    <Activityundefined {...props} />
+  ),
+  AddUserundefined: (props: SvgProps) => (
+    <AddUserundefined {...props} />
+  ),
+  Avatarundefined: (props: SvgProps) => (
+    <Avatarundefined {...props} />
+  ),
+  Bag_22: (props: SvgProps) => (
+    <Bag_22 {...props} />
+  ),
+  Bag_33: (props: SvgProps) => (
+    <Bag_33 {...props} />
+  ),
+  Bagundefined: (props: SvgProps) => (
+    <Bagundefined {...props} />
+  ),
+  Bookmarkundefined: (props: SvgProps) => (
+    <Bookmarkundefined {...props} />
+  ),
+  Buyundefined: (props: SvgProps) => (
+    <Buyundefined {...props} />
+  ),
+  Calendarundefined: (props: SvgProps) => (
+    <Calendarundefined {...props} />
+  ),
+  CallMissedundefined: (props: SvgProps) => (
+    <CallMissedundefined {...props} />
+  ),
+  CallSilentundefined: (props: SvgProps) => (
+    <CallSilentundefined {...props} />
+  ),
+  Callundefined: (props: SvgProps) => (
+    <Callundefined {...props} />
+  ),
+  Callingundefined: (props: SvgProps) => (
+    <Callingundefined {...props} />
+  ),
+  Cameraundefined: (props: SvgProps) => (
+    <Cameraundefined {...props} />
+  ),
+  Cardundefined: (props: SvgProps) => (
+    <Cardundefined {...props} />
+  ),
+  Categoryundefined: (props: SvgProps) => (
+    <Categoryundefined {...props} />
+  ),
+  Chartundefined: (props: SvgProps) => (
+    <Chartundefined {...props} />
+  ),
+  Chat_22: (props: SvgProps) => (
+    <Chat_22 {...props} />
+  ),
+  Chatundefined: (props: SvgProps) => (
+    <Chatundefined {...props} />
+  ),
+  CloseSquareundefined: (props: SvgProps) => (
+    <CloseSquareundefined {...props} />
+  ),
+  DangerCircleundefined: (props: SvgProps) => (
+    <DangerCircleundefined {...props} />
+  ),
+  DangerTriangleundefined: (props: SvgProps) => (
+    <DangerTriangleundefined {...props} />
+  ),
+  Delete_22: (props: SvgProps) => (
+    <Delete_22 {...props} />
+  ),
+  Deleteundefined: (props: SvgProps) => (
+    <Deleteundefined {...props} />
+  ),
+  Discountundefined: (props: SvgProps) => (
+    <Discountundefined {...props} />
+  ),
+  Discoveryundefined: (props: SvgProps) => (
+    <Discoveryundefined {...props} />
+  ),
+  Documentundefined: (props: SvgProps) => (
+    <Documentundefined {...props} />
+  ),
+  Download_11: (props: SvgProps) => (
+    <Download_11 {...props} />
+  ),
+  Downloadundefined: (props: SvgProps) => (
+    <Downloadundefined {...props} />
+  ),
+  EditSquareundefined: (props: SvgProps) => (
+    <EditSquareundefined {...props} />
+  ),
+  Editundefined: (props: SvgProps) => (
+    <Editundefined {...props} />
+  ),
+  Emojiundefined: (props: SvgProps) => (
+    <Emojiundefined {...props} />
+  ),
+  Filter_33: (props: SvgProps) => (
+    <Filter_33 {...props} />
+  ),
+  Filterundefined: (props: SvgProps) => (
+    <Filterundefined {...props} />
+  ),
+  Folderundefined: (props: SvgProps) => (
+    <Folderundefined {...props} />
+  ),
+  Gameundefined: (props: SvgProps) => (
+    <Gameundefined {...props} />
+  ),
+  Graphundefined: (props: SvgProps) => (
+    <Graphundefined {...props} />
+  ),
+  Heartundefined: (props: SvgProps) => (
+    <Heartundefined {...props} />
+  ),
+  Heart22: (props: SvgProps) => (
+    <Heart22 {...props} />
+  ),
+  Hideundefined: (props: SvgProps) => (
+    <Hideundefined {...props} />
+  ),
+  Home_22: (props: SvgProps) => (
+    <Home_22 {...props} />
+  ),
+  Homeundefined: (props: SvgProps) => (
+    <Homeundefined {...props} />
+  ),
+  Image_33: (props: SvgProps) => (
+    <Image_33 {...props} />
+  ),
+  Imageundefined: (props: SvgProps) => (
+    <Imageundefined {...props} />
+  ),
+  InfoSquareundefined: (props: SvgProps) => (
+    <InfoSquareundefined {...props} />
+  ),
+  Location_22: (props: SvgProps) => (
+    <Location_22 {...props} />
+  ),
+  Locationundefined: (props: SvgProps) => (
+    <Locationundefined {...props} />
+  ),
+  Lockundefined: (props: SvgProps) => (
+    <Lockundefined {...props} />
+  ),
+  Login_11: (props: SvgProps) => (
+    <Login_11 {...props} />
+  ),
+  Loginundefined: (props: SvgProps) => (
+    <Loginundefined {...props} />
+  ),
+  Logout_11: (props: SvgProps) => (
+    <Logout_11 {...props} />
+  ),
+  Logoutundefined: (props: SvgProps) => (
+    <Logoutundefined {...props} />
+  ),
+  Messageundefined: (props: SvgProps) => (
+    <Messageundefined {...props} />
+  ),
+  MoreCircleundefined: (props: SvgProps) => (
+    <MoreCircleundefined {...props} />
+  ),
+  MoreSquareundefined: (props: SvgProps) => (
+    <MoreSquareundefined {...props} />
+  ),
+  Notification_22: (props: SvgProps) => (
+    <Notification_22 {...props} />
+  ),
+  Notification_33: (props: SvgProps) => (
+    <Notification_33 {...props} />
+  ),
+  Notificationundefined: (props: SvgProps) => (
+    <Notificationundefined {...props} />
+  ),
+  PaperDownload_11: (props: SvgProps) => (
+    <PaperDownload_11 {...props} />
+  ),
+  PaperDownloadundefined: (props: SvgProps) => (
+    <PaperDownloadundefined {...props} />
+  ),
+  PaperFailundefined: (props: SvgProps) => (
+    <PaperFailundefined {...props} />
+  ),
+  PaperNegativeundefined: (props: SvgProps) => (
+    <PaperNegativeundefined {...props} />
+  ),
+  PaperPlusundefined: (props: SvgProps) => (
+    <PaperPlusundefined {...props} />
+  ),
+  PaperUpload_11: (props: SvgProps) => (
+    <PaperUpload_11 {...props} />
+  ),
+  PaperUploadundefined: (props: SvgProps) => (
+    <PaperUploadundefined {...props} />
+  ),
+  Paperundefined: (props: SvgProps) => (
+    <Paperundefined {...props} />
+  ),
+  Passwordundefined: (props: SvgProps) => (
+    <Passwordundefined {...props} />
+  ),
+  Playundefined: (props: SvgProps) => (
+    <Playundefined {...props} />
+  ),
+  Plusundefined: (props: SvgProps) => (
+    <Plusundefined {...props} />
+  ),
+  Profileundefined: (props: SvgProps) => (
+    <Profileundefined {...props} />
+  ),
+  QRundefined: (props: SvgProps) => (
+    <QRundefined {...props} />
+  ),
+  Scanundefined: (props: SvgProps) => (
+    <Scanundefined {...props} />
+  ),
+  Searchundefined: (props: SvgProps) => (
+    <Searchundefined {...props} />
+  ),
+  Sendundefined: (props: SvgProps) => (
+    <Sendundefined {...props} />
+  ),
+  Setting_22: (props: SvgProps) => (
+    <Setting_22 {...props} />
+  ),
+  Settingundefined: (props: SvgProps) => (
+    <Settingundefined {...props} />
+  ),
+  ShieldDoneundefined: (props: SvgProps) => (
+    <ShieldDoneundefined {...props} />
+  ),
+  ShieldFailundefined: (props: SvgProps) => (
+    <ShieldFailundefined {...props} />
+  ),
+  Showundefined: (props: SvgProps) => (
+    <Showundefined {...props} />
+  ),
+  Star_22: (props: SvgProps) => (
+    <Star_22 {...props} />
+  ),
+  Starundefined: (props: SvgProps) => (
+    <Starundefined {...props} />
+  ),
+  Swapundefined: (props: SvgProps) => (
+    <Swapundefined {...props} />
+  ),
+  TickSquareundefined: (props: SvgProps) => (
+    <TickSquareundefined {...props} />
+  ),
+  TicketStarundefined: (props: SvgProps) => (
+    <TicketStarundefined {...props} />
+  ),
+  Ticketundefined: (props: SvgProps) => (
+    <Ticketundefined {...props} />
+  ),
+  TimeCircleundefined: (props: SvgProps) => (
+    <TimeCircleundefined {...props} />
+  ),
+  TimeSquareundefined: (props: SvgProps) => (
+    <TimeSquareundefined {...props} />
+  ),
+  Unlockundefined: (props: SvgProps) => (
+    <Unlockundefined {...props} />
+  ),
+  Upload_11: (props: SvgProps) => (
+    <Upload_11 {...props} />
+  ),
+  Uploadundefined: (props: SvgProps) => (
+    <Uploadundefined {...props} />
+  ),
+  Videoundefined: (props: SvgProps) => (
+    <Videoundefined {...props} />
+  ),
+  Voice_33: (props: SvgProps) => (
+    <Voice_33 {...props} />
+  ),
+  Voiceundefined: (props: SvgProps) => (
+    <Voiceundefined {...props} />
+  ),
+  VolumeDownundefined: (props: SvgProps) => (
+    <VolumeDownundefined {...props} />
+  ),
+  VolumeOffundefined: (props: SvgProps) => (
+    <VolumeOffundefined {...props} />
+  ),
+  VolumeUpundefined: (props: SvgProps) => (
+    <VolumeUpundefined {...props} />
+  ),
+  Walletundefined: (props: SvgProps) => (
+    <Walletundefined {...props} />
+  ),
+  Workundefined: (props: SvgProps) => (
+    <Workundefined {...props} />
+  ),
+};
+
